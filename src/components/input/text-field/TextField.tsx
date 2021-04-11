d4b7713fc55ca23262cebeaacbfce4cb7c1c9477
Jorge Moreira <jorge.smrr@gmail.com>
2021-04-11 14:48:11 -0300

    Fix TextField classes and Spinner styles

--- src/components/input/text-field/TextField.tsx
+++ src/components/input/text-field/TextField.tsx
@@ -1,45 +1,51 @@
 import React from "react";
 
 export interface TextFieldProps {
-  value?: string | number;
-  id?: string;
-  name?: string;
-  placeholder?: string;
-  disabled?: boolean;
-  error?: boolean;
-  autoCleanErrors?: boolean;
-  type?: string;
-  onChange?: Function;
+    value?: string | number;
+    id?: string;
+    name?: string;
+    placeholder?: string;
+    disabled?: boolean;
+    error?: boolean;
+    autoCleanErrors?: boolean;
+    type?: string;
+    onChange?: Function;
+    className?: string;
 }
 
 class TextField extends React.Component<TextFieldProps> {
-  static defaultProps = {
-    type: "text",
-    autoCleanErrors: true,
-  };
+    static defaultProps = {
+        type: "text",
+        autoCleanErrors: true,
+    };
 
-  onChange(event: React.FormEvent<HTMLInputElement>) {
-    if (this.props.error && this.props.autoCleanErrors) {
-      // todo clear error
+    onChange(event: React.FormEvent<HTMLInputElement>) {
+        if (this.props.error && this.props.autoCleanErrors) {
+            // todo clear error
+        }
+
+        this.props.onChange?.((event.target as HTMLInputElement).value);
     }
 
-    this.props.onChange?.((event.target as HTMLInputElement).value);
-  }
+    render() {
+        let className = this.props.className || "";
+        if (this.props.error) {
+            className += " field-error";
+        }
 
-  render() {
-    return (
-      <input
-        id={this.props.id}
-        name={this.props.name}
-        type={this.props.type}
-        placeholder={this.props.placeholder}
-        value={this.props.value}
-        className={this.props.error ? "field-error" : ""}
-        disabled={this.props.disabled}
-        onChange={(ev) => this.onChange(ev)}
-      />
-    );
-  }
+        return (
+            <input
+                id={this.props.id}
+                name={this.props.name}
+                type={this.props.type}
+                placeholder={this.props.placeholder}
+                value={this.props.value}
+                className={className}
+                disabled={this.props.disabled}
+                onChange={(ev) => this.onChange(ev)}
+            />
+        );
+    }
 }
 
 export default TextField;
