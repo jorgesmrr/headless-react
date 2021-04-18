329f717dbdde41ce973f1980ed4ff1b225a24471
Jorge Moreira <jorge.smrr@gmail.com>
2021-04-18 12:19:22 -0300

    Remove px units

--- src/components/navigation/drawer/Drawer.tsx
+++ src/components/navigation/drawer/Drawer.tsx
@@ -1,23 +1,22 @@
 import React, { FunctionComponent } from "react";
 
 interface Props {
-  open: boolean;
-  handleClose: Function;
+    open: boolean;
+    handleClose: Function;
 }
 
 const Drawer: FunctionComponent<Props> = ({ open, handleClose, children }) => (
-  <div>
-    {open && <div className="drawer-overlay" onClick={() => handleClose()} />}
+    <div>
+        {open && (
+            <div className="drawer-overlay" onClick={() => handleClose()} />
+        )}
 
-    <div
-      className={`drawer ${open ? "open" : null}`}
-      style={{ width: "250px" }}
-    >
-      <h4 className="px-4">Menu</h4>
+        <div className={`drawer ${open ? "open" : null}`}>
+            <h4 className="px-4">Menu</h4>
 
-      <ul>{children}</ul>
+            <ul>{children}</ul>
+        </div>
     </div>
-  </div>
 );
 
 export default Drawer;
