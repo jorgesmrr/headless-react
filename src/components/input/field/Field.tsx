7d13589cfdef0c26258ed20951f4b96dffaa2c3e
Jorge Moreira <jorge.smrr@gmail.com>
2021-04-21 16:15:09 -0300

    Fix Field props

--- src/components/input/field/Field.tsx
+++ src/components/input/field/Field.tsx
@@ -1,39 +1,38 @@
 import React from "react";
 
 export interface FieldProps {
-  for?: string;
-  label?: string;
-  hint?: string;
-  error?: string | boolean;
+    for?: string;
+    label?: string;
+    hint?: string;
+    error?: string;
 }
 
 class Field extends React.Component<FieldProps> {
-  render() {
-    const showErrorMessage =
-      this.props.error && typeof this.props.error === "string";
+    render() {
+        return (
+            <div className="field">
+                {this.props.label && (
+                    <label htmlFor={this.props.for}>
+                        {this.props.label}
 
-    return (
-      <div className="field">
-        {this.props.label && (
-          <label htmlFor={this.props.for}>
-            {this.props.label}
+                        {this.props.hint && <br />}
 
-            {this.props.hint && <br />}
+                        {this.props.hint && (
+                            <small className="field-hint">
+                                {this.props.hint}
+                            </small>
+                        )}
+                    </label>
+                )}
 
-            {this.props.hint && (
-              <small className="field-hint">{this.props.hint}</small>
-            )}
-          </label>
-        )}
+                {this.props.children}
 
-        {this.props.children}
-
-        {showErrorMessage && (
-          <small className="field-error">{this.props.error}</small>
-        )}
-      </div>
-    );
-  }
+                {this.props.error && (
+                    <small className="field-error">{this.props.error}</small>
+                )}
+            </div>
+        );
+    }
 }
 
 export default Field;
