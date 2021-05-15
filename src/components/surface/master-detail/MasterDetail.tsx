d177e1e0efa1a6a92c6d3fb3550f83165e8e3d4b
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-15 16:58:59 -0300

    Allow some customization in MasterDetail

--- src/components/surface/master-detail/MasterDetail.tsx
+++ src/components/surface/master-detail/MasterDetail.tsx
@@ -5,13 +5,17 @@ export interface MasterDetailProps {
     master: JSX.Element;
     detail: JSX.Element;
     showMaster: boolean;
+    masterWidth?: string;
+    maxContentZIndex?: number;
     onHideMaster: () => void;
 }
 
 const MasterDetail: React.FC<MasterDetailProps> = ({
     master,
     detail,
+    masterWidth = "21rem",
     showMaster = true,
+    maxContentZIndex = 1,
     onHideMaster,
 }) => {
     const className = classNames("master-detail", {
@@ -20,9 +24,27 @@ const MasterDetail: React.FC<MasterDetailProps> = ({
 
     return (
         <div className={className}>
-            <div className="master-detail__master">{master}</div>
-            <div className="master-detail__detail">{detail}</div>
-            <div className="master-detail__overlay" onClick={onHideMaster} />
+            <div
+                className="master-detail__master"
+                style={{
+                    zIndex: maxContentZIndex + 2,
+                    width: `min(80vw, ${masterWidth})`,
+                    flex: `0 0 ${masterWidth}`,
+                }}
+            >
+                {master}
+            </div>
+            <div
+                className="master-detail__detail"
+                style={{ zIndex: maxContentZIndex }}
+            >
+                {detail}
+            </div>
+            <div
+                className="master-detail__overlay"
+                style={{ zIndex: maxContentZIndex + 1 }}
+                onClick={onHideMaster}
+            />
         </div>
     );
 };
--- src/stories/surface/MasterDetail.stories.tsx
+++ src/stories/surface/MasterDetail.stories.tsx
@@ -5,17 +5,20 @@ import MasterDetail, {
 } from "../../components/surface/master-detail/MasterDetail";
 
 export default {
-    title: "Message/MasterDetail",
+    title: "Surface/MasterDetail",
     component: MasterDetail,
 };
 
 const Template: Story<MasterDetailProps> = (args: MasterDetailProps) => (
-    <MasterDetail {...args} />
+    <div style={{ height: "100vh" }}>
+        <MasterDetail {...args} />
+    </div>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-    master: <div>Master</div>,
-    detail: <div>Detail</div>,
+    master: <div style={{ backgroundColor: "yellow" }}>Master</div>,
+    detail: <div style={{ backgroundColor: "grey" }}>Detail</div>,
     showMaster: true,
+    masterWidth: "30rem",
 };
